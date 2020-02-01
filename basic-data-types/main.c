86eff0c1c74f178743298a77759813bfee34933b
Thanos <kthanoss90@gmail.com>
2020-02-01 16:03:31 +0100

    basic data-types

---
+++ basic-data-types/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int integerNumber = 10;
+    float floatNumber = 10.5;
+    double doubleNumber = 10.723462356776874;
+
+    // c89 standard
+    _Bool trueBooleanC89 = 1;
+    _Bool falseBooleanC89 = 0;
+
+    // c99 standard
+    bool trueBooleanC99 = true;
+    bool falseBooleanC99 = false;
+
+
+    return 0;
+}
+
