10fd26b6dfbfc679775286eee6b6b75af970de53
Thanos <kthanoss90@gmail.com>
2020-07-26 12:16:04 +0200

    characters

--- 02-variables-and-datatypes/enums-and-chars/main.c
+++ 02-variables-and-datatypes/enums-and-chars/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Enums and Chars\n");
-    printf("\n");
-
     // declaire an Enum
     enum primaryColor {
         red,
@@ -16,10 +13,22 @@ int main() {
     // assign variable only one of the enum types
     // the enum is a list of integers so the code below
     // should print 0,1,2 ...
+    printf("Enum\n");
+    printf("\n");
     myColor = red;
     printf("%u\n", myColor);
     myColor = yellow;
     printf("%u\n", myColor);
     myColor = blue;
     printf("%u\n", myColor);
+    printf("\n");
+
+    printf("Characters\n");
+    printf("\n");
+    // declaire a character
+    char thanos;
+    // have to assign it using single quotes
+    // double quotes are used for assigning strings
+    thanos = 't';
+    printf("%u\n", thanos);
 }
