071ef4bca09211052e2f28769d9daf7989db1862
Thanos <kthanoss90@gmail.com>
2020-07-26 12:06:40 +0200

    enums

--- 02-variables-and-datatypes/enums-and-chars/main.c
+++ 02-variables-and-datatypes/enums-and-chars/main.c
@@ -2,4 +2,24 @@
 
 int main() {
     printf("Enums and Chars\n");
+    printf("\n");
+
+    // declaire an Enum
+    enum primaryColor {
+        red,
+        yellow,
+        blue,
+    };
+
+    // declaire variable of enum type
+    enum primaryColor myColor;
+    // assign variable only one of the enum types
+    // the enum is a list of integers so the code below
+    // should print 0,1,2 ...
+    myColor = red;
+    printf("%u\n", myColor);
+    myColor = yellow;
+    printf("%u\n", myColor);
+    myColor = blue;
+    printf("%u\n", myColor);
 }
