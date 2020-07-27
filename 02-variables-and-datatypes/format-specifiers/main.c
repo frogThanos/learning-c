6c9c3ebab6513c986300150eb609b8670fb72e99
Thanos <kthanoss90@gmail.com>
2020-07-27 21:31:44 +0200

    bool C89 vs C99

--- 02-variables-and-datatypes/format-specifiers/main.c
+++ 02-variables-and-datatypes/format-specifiers/main.c
@@ -12,12 +12,15 @@ int main() {
 
     // there is no format specifier for the C99 bool type :(
     // we need to use the _Bool from C89
-    _Bool boolVar = 1;
+    _Bool boolC89Var = 1;
+    // we can however assing the bool a 1 instead of true
+    bool boolC99Var = 0;
 
     printf("The integerVar is %i\n", integerVar);
     printf("The floatingVar is %f\n", floatingVar);
     printf("The doubleVar is %e\n", doubleVar);
     printf("The doubleVar is %g\n", doubleVar);
     printf("The charVar is %c\n", charVar);
-    printf("The boolVar is %i\n", boolVar);
+    printf("The boolC89Var is %i\n", boolC89Var);
+    printf("The boolC99Var is %i\n", boolC99Var);
 }
